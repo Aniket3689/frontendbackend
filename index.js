f7d267b0adef31b913bf629586280cea595da0dc
Aniket3689 <aniket.bcastudent21.6607@cimage.in>
2024-04-29 09:35:58 +0530

    add jokes array

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
 const express = require("express");
 const app=express();
 
-app.get("/",(req,res)=>{
-    res.send("app get aniket")
+app.get("/jokes",(req,res)=>{
+    const Jokes=[
+        {
+            id:1,
+            name:"aniket",
+            title:"my Jokes"
+        },
+        {
+            id:2,
+            name:"aniket 2",
+            title:"my Jokes 2"
+        },
+        {
+            id:3,
+            name:"aniket 3",
+            title:"my Jokes 3"
+        },
+        {
+            id:4,
+            name:"aniket 4",
+            title:"my Jokes 4"
+        },
+        {
+            id:5,
+            name:"aniket 5",
+            title:"my Jokes 5"
+        },
+
+    ]
+    res.send(Jokes);
 });
 const port=process.env.PORT ||4000
 
